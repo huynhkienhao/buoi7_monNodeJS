afe5ca207ff63ec2dde464321399ded7aac9018f
Huynh Kien Hao <kienhao2209@gmail.com>
2025-03-20 16:11:44 +0700

    first commit

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,8 @@ const { ConnectionCheckOutFailedEvent } = require('mongodb');
 var router = express.Router();
 let productModel = require('../schemas/product')
 let CategoryModel = require('../schemas/category')
+let { check_authentication, check_authorization } = require('../utils/check_auth');
+let constants = require('../utils/constants');
 
 function buildQuery(obj){
   console.log(obj);
@@ -31,120 +33,72 @@ function buildQuery(obj){
 }
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  
-
+router.get('/', async function (req, res, next) {
   let products = await productModel.find(buildQuery(req.query)).populate("category");
-
-  res.status(200).send({
-    success:true,
-    data:products
-  });
-});
-router.get('/:id', async function(req, res, next) {
-  try {
-    let id = req.params.id;
-    let product = await productModel.findById(id);
-    res.status(200).send({
-      success:true,
-      data:product
-    });
-  } catch (error) {
-    res.status(404).send({
-      success:false,
-      message:"khong co id phu hop"
-    });
-  }
+  res.status(200).send({ success: true, data: products });
 });
 
-router.post('/', async function(req, res, next) {
-  try {
-    let cate = await CategoryModel.findOne({name:req.body.category})
-    if(cate){
-      let newProduct = new productModel({
-        name: req.body.name,
-        price:req.body.price,
-        quantity: req.body.quantity,
-        category:cate._id
-      })
-      await newProduct.save();
-      res.status(200).send({
-        success:true,
-        data:newProduct
-      });
-    }else{
-      res.status(404).send({
-        success:false,
-        data:"cate khong dung"
-      });
-    }
-  } catch (error) {
-    res.status(404).send({
-      success:false,
-      message:error.message
-    });
-  }
-});
-router.put('/:id', async function(req, res, next) {
-  try {
-    let updateObj = {};
-    let body = req.body;
-    if(body.name){
-      updateObj.name = body.name;
-    }
-    if(body.price){
-      updateObj.price = body.price;
-    }
-    if(body.quantity){
-      updateObj.quantity = body.quantity;
-    }
-    if(body.category){
-      let cate = await CategoryModel.findOne({name:req.body.category});
-      if(!cate){
-        res.status(404).send({
-          success:false,
-          message:error.message
-        });
-      }
-    }
-    let updatedProduct = await productModel.findByIdAndUpdate(req.params.id,
-      updateObj,
-      {new:true})
-    res.status(200).send({
-      success:true,
-      data:updatedProduct
-    });
-  } catch (error) {
-    res.status(404).send({
-      success:false,
-      message:error.message
-    });
-  }
-});
-router.delete('/:id', async function(req, res, next) {
+router.get('/:id', async function (req, res, next) {
   try {
     let product = await productModel.findById(req.params.id);
-    if(product){
-      let deletedProduct = await productModel.findByIdAndUpdate(req.params.id,
-        {
-          isDeleted:true
-        },
-        {new:true})
-        res.status(200).send({
-          success:true,
-          data:deletedProduct
-        });
-    }else{
-      res.status(404).send({
-        success:false,
-        message:"ID khong ton tai"
-      });
-    }
+    res.status(200).send({ success: true, data: product });
   } catch (error) {
-    res.status(404).send({
-      success:false,
-      message:error.message
-    });
+    res.status(404).send({ success: false, message: "Không có ID phù hợp" });
   }
 });
+
+// POST - Yêu cầu quyền MOD
+router.post('/', check_authentication, check_authorization(constants.MOD_PERMISSION), async function (req, res, next) {
+  try {
+    let cate = await CategoryModel.findOne({ name: req.body.category });
+    if (cate) {
+      let newProduct = new productModel({
+        name: req.body.name,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        category: cate._id,
+      });
+      await newProduct.save();
+      res.status(200).send({ success: true, data: newProduct });
+    } else {
+      res.status(404).send({ success: false, message: "Danh mục không hợp lệ" });
+    }
+  } catch (error) {
+    res.status(404).send({ success: false, message: error.message });
+  }
+});
+
+// PUT - Yêu cầu quyền MOD
+router.put('/:id', check_authentication, check_authorization(constants.MOD_PERMISSION), async function (req, res, next) {
+  try {
+    let updateObj = { ...req.body };
+    if (req.body.category) {
+      let cate = await CategoryModel.findOne({ name: req.body.category });
+      if (!cate) {
+        return res.status(404).send({ success: false, message: "Danh mục không hợp lệ" });
+      }
+      updateObj.category = cate._id;
+    }
+    let updatedProduct = await productModel.findByIdAndUpdate(req.params.id, updateObj, { new: true });
+    res.status(200).send({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(404).send({ success: false, message: error.message });
+  }
+});
+
+// DELETE - Yêu cầu quyền ADMIN
+router.delete('/:id', check_authentication, check_authorization(constants.ADMIN_PERMISSION), async function (req, res, next) {
+  try {
+    let product = await productModel.findById(req.params.id);
+    if (product) {
+      let deletedProduct = await productModel.findByIdAndUpdate(req.params.id, { isDeleted: true }, { new: true });
+      res.status(200).send({ success: true, data: deletedProduct });
+    } else {
+      res.status(404).send({ success: false, message: "ID không tồn tại" });
+    }
+  } catch (error) {
+    res.status(404).send({ success: false, message: error.message });
+  }
+});
+
 module.exports = router;
